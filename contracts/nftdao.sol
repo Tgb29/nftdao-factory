cce7be8840175703de65ddedb5e24e3b73bc45c9
theodorebrinkofski <tgb29@cornell.edu>
2022-04-09 12:00:29 -0400

    last draft before testing

--- contracts/nftdao.sol
+++ contracts/nftdao.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract PutinPoker is ERC721{
+contract NFTDAO is ERC721{
     
     using Strings for uint256;
 
@@ -18,38 +18,50 @@ contract PutinPoker is ERC721{
 
     //DAO variables
     address public owner;
-
     struct Proposal {
         uint id;
         address proposer;
         uint amount;
         uint description;
-        uint startTime;
-        uint endTime;
+        uint startBlock;
+        uint endBlock;
         uint yesVotes;
         uint noVotes;
         bool isLive;
         bool isPassed;
-        uint proposalType;
+        bool isExecuted;
     }
-
-    //types == money, owner, voteThreshold
-
     mapping (uint => Proposal) public proposals;
+    mapping (address => uint) public lastProposal;
+    mapping (address => uint[]) public proposerHistory;
+    mapping(uint => mapping(address => uint)) public voterHistory;
 
-    uint public voteThreshold;
+    uint public totalVotesThreshold;
+    uint public voterThreshold = 1;
+    uint public proposalThreshold;
+
     uint public totalProposals;
     uint public proposalsPassed;
+    uint public blocksPerDay = 6500;
 
 
-    constructor(string memory  _name, string memory _symbol, uint _maxSupply, uint _voteThreshold) ERC721(_name,  _symbol) {
-        maxSupply = _maxSupply
+    constructor(string memory  _name, string memory _symbol, uint _maxSupply, uint _totalVotesThreshold, uint _proposalThreshold) ERC721(_name,  _symbol) {
+        maxSupply = _maxSupply;
         owner = msg.sender;
-        voteThreshold = _voteThreshold
+        totalVotesThreshold = _totalVotesThreshold;
+        proposalThreshold = _proposalThreshold;
     }
 
     //events
     event Mint(address indexed _from, uint _id);
+
+    event ProposalCreated(address indexed _from, uint _id);
+
+    event ProposalExecuted(bool _passed, address indexed _to, uint _amount, uint _id);
+
+    event VoteCast(address indexed _from, bool _yes, uint _id);
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
 
     //mint
     function mint(uint _mintAmount) public payable {
@@ -83,13 +95,87 @@ contract PutinPoker is ERC721{
     }
 
     //DAO
-
     modifier onlyOwner() {
         require(msg.sender==owner);
         _;
     }
 
+    function transferOwner(address _newOwner) onlyOwner{
+        OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
+
     //propose
+    function propose (uint _amount, string memory _ipfsHash) public {
+        require(balanceOf(msg.sender) >= proposalThreshold, "Use doesn't have enough NFTs to create proposal");
+        require(lastProposal[msg.sender] == 0, "Only 1 proposal per user");
+
+        Proposal memory _proposal;
+
+        _proposal.id = totalProposals+1;
+        _proposal.proposer = msg.sender;
+        _proposal.amount = _amount;
+        _proposal.description = _ipfsHash;
+        _proposal.startBlock = block.number;
+        _proposal.endBlock = block.number + (blocksPerDay*7);
+        _proposal.isLive = true;
+
+        proposals[_proposal.id] = _proposal;
+        lastProposal[msg.sender] = _propsal.id;
+        totalProposals +=1;
+        proposerHistory[msg.sender].push(_propsal.id);
+
+        emit ProposalCreated(msg.sender, _propsal.id);
+
+    }
+    //settle proposal
+    function executeProposal(uint _proposalId) public {
+        require(block.number > proposals[_proposalId].endBlock, "Proposal still open");
+        require(proposals[_proposalId].isExecuted == false, "Proposal already executed");
+        require(proposals[_proposalId].id != 0, "Proposal doesn't exist");
+
+        proposals[_proposalId].isLive = false;
+        uint totalVotes = proposals[_proposalId].yesVotes + proposals[_proposalId].noVotes;
+        if (totalVotes <= totalVotesThreshold) {
+            proposals[_proposalId].isExecuted = true;
+            del lastProposal[msg.sender];
+            emit ProposalExecuted(false, owner, 0, _proposalId);
+        } else {
+            if (proposals[_proposalId].yesVotes > proposals[_proposalId].noVotes) {
+                proposals[_proposalId].isPassed = true;
+                require(address(this).balance > proposals[_proposalId].amount, "Contract doesn't have enough funds for proposal");
+                (bool sent, bytes memory data) = _to.call{value: proposals[_proposalId].amount}("");
+                require(sent, "Failed to send Ether");
+                proposals[_proposalId].isExecuted = true;
+                del lastProposal[msg.sender];
+                emit ProposalExecuted(true, owner, proposals[_proposalId].amount, _proposalId);
+
+            } else {
+                proposals[_proposalId].isExecuted = true;
+                del lastProposal[msg.sender];
+                emit ProposalExecuted(false, owner, 0, _proposalId);
+            }
+        }
+        
+    }
+
     //vote
+    function vote(bool _yes, uint _proposalId) {
+        require(balanceOf(msg.sender) >= voterThreshold, "Use doesn't have enough NFTs to vote");
+        require(proposals[_proposalId].isLive, "Proposal is closed");
+        require(block.number < proposals[_proposalId].endBlock, "Voting window is closed");
+        require(voterHistory[_proposalId][msg.sender] == false, "User already voted");
+
+        if (_yes) {
+            proposals[_proposalId].yesVotes +=1;
+            emit VoteCast(msg.sender, true, _proposalId);
+        } else {
+            proposals[_proposalId].noVotes +=1;
+            emit VoteCast(msg.sender, false, _proposalId);
+        }
+
+        voterHistory[_proposalId][msg.sender] == true;
+
+    }
     
 }
